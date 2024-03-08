93bc21f74a6173cf5c2500e19cb950df9be858e5
gary <gary_1118@impacts.cloud>
2024-03-08 17:03:29 +0800

    modify config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  ssr: false,
   app: {
-    baseURL: process.env.NODE_ENV === 'production' ? '/nuxt3-generate/' : '/',
+    baseURL: process.env.NODE_ENV === 'production' ? '/slot-machine/' : '/',
     buildAssetsDir: '/static/'
   }
 })
